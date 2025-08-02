e0e55049e5886b378e8b6fa2544d3f00e31aae29
josemar <64178122+josemar14@users.noreply.github.com>
2025-08-01 21:56:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Token_inspector/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
-  }
+  base: '/Token_inspector/', // Caminho base para GitHub Pages
 })
