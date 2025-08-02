5d97147d9d38556e7a22e1b5b8f891d2295a02ff
josemar <64178122+josemar14@users.noreply.github.com>
2025-08-01 21:17:11 -0300

    Update and rename vite.config.ts to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Token_inspector/',
   plugins: [react()],
-  base: './',
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
     }
   }
 })
+
